9b21dd86019ab88bc69fb0282b4d0ac38e93aab9
Simão Pedro <simaaoweb@gmail.com>
2025-05-14 12:02:24 +0100

    DESCARGA SUBMIT FORM FIXED

--- script.js
+++ script.js
@@ -353,14 +353,9 @@ deleteDescarga.addEventListener('click', () => {
 // EMAIL SUBJECT AND TITLES TO DESCARGA
 
 submitD.addEventListener('click', (event) => {
-        //event.preventDefault()
-
         if (teamD.value !== '') {
        subjectD.value = `DESCARGA do ${teamD.value} de ${clientD.value}`;
    }
-  
- deleteDescargaFunction()
-
 })
 
 // PRINT INTERVENÇÃO
